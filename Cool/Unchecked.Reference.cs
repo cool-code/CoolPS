b8727ef1c2d9c79ff133f3fe2e88fe495e957fce
andot <mabingyao@gmail.com>
2026-05-22 21:28:11 +0800

    Add .NET Framework reference helpers and tests
    Introduce platform-specific reference helpers for .NET Framework and unify Unchecked reference APIs. Adds Cool/ReferenceExtensions.NetFx.cs (NETFRAMEWORK) providing aggressive-inlining implementations of string.GetPinnableReference and MemoryMarshal.GetArrayDataReference. Remove older platform-specific Unchecked.Reference.Net.cs and Unchecked.Reference.NetFx.cs files and update Cool/Unchecked.Reference.cs to use MemoryMarshal.GetReference/GetArrayDataReference and provide new overloads: string, T[], Array, and unmanaged pointer variants; also replace DangerousGetPinnableReference usage. Add comprehensive tests in Tests/Cool.Tests/ReferenceTests.cs covering string/array pinnable references, empty/null cases, multi-dimensional and non-zero-based arrays, and Unchecked reference behavior.

--- Cool/Unchecked.Reference.cs
+++ Cool/Unchecked.Reference.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Cool;
 
@@ -6,15 +8,24 @@ namespace Cool;
 public static partial class Unchecked
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref T GetReference<T>(in Span<T> span) => ref span.DangerousGetPinnableReference();
+    public static ref T GetReference<T>(in Span<T> span) => ref MemoryMarshal.GetReference<T>(span);
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref T GetReference<T>(in ReadOnlySpan<T> span) => ref span.DangerousGetPinnableReference();
+    public static ref T GetReference<T>(in ReadOnlySpan<T> span) => ref MemoryMarshal.GetReference<T>(span);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref T GetReference<T>(in System.Span<T> span) => ref ((Span<T>)span).DangerousGetPinnableReference();
+    public static ref T GetReference<T>(in System.Span<T> span) => ref MemoryMarshal.GetReference<T>((Span<T>)span);
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref T GetReference<T>(in System.ReadOnlySpan<T> span) => ref ((ReadOnlySpan<T>)span).DangerousGetPinnableReference();
+    public static ref T GetReference<T>(in System.ReadOnlySpan<T> span) => ref MemoryMarshal.GetReference<T>((ReadOnlySpan<T>)span);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe ref char GetReference(char* buffer) => ref Unsafe.AsRef<char>(buffer);
+    public static ref char GetReference(string str) => ref Unsafe.AsRef(in str.GetPinnableReference());
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref T GetReference<T>(T[] array) => ref MemoryMarshal.GetArrayDataReference(array);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref T GetReference<T>(Array array) => ref Unsafe.As<byte, T>(ref MemoryMarshal.GetArrayDataReference(array));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe ref T GetReference<T>(T* buffer) where T : unmanaged => ref Unsafe.AsRef<T>(buffer);
 }
---
+++ Tests/Cool.Tests/ReferenceTests.cs
@@ -0,0 +1,207 @@
+using System;
+using Xunit;
+using Cool;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Cool.Tests
+{
+#pragma warning disable CS8604, CS8625, CS8601, CS8602
+    public class ReferenceTests
+    {
+        [Fact]
+        public void String_GetPinnableReference()
+        {
+            string str = "Hello, World!";
+            ref readonly char c = ref str.GetPinnableReference();
+            Assert.Equal('H', c);
+        }
+
+        [Fact]
+        public void NullString_GetPinnableReference()
+        {
+            string? str = null;
+            Assert.Throws<NullReferenceException>(() => { ref readonly char c = ref str.GetPinnableReference(); });
+        }
+
+        [Fact]
+        public void EmptyString_GetPinnableReference()
+        {
+            string str = "";
+            ref readonly char c = ref str.GetPinnableReference();
+            Assert.Equal('\0', c); // GetPinnableReference on empty string returns reference to null terminator
+        }
+
+        [Fact]
+        public void Array_GetArrayDataReference()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            ref int r = ref MemoryMarshal.GetArrayDataReference(arr);
+            Assert.Equal(1, r);
+        }
+
+        [Fact]
+        public void EmptyArray_GetArrayDataReference()
+        {
+            int[] arr = [];
+            ref int r = ref MemoryMarshal.GetArrayDataReference(arr);
+            Assert.Equal(0, r); // GetArrayDataReference on empty array returns reference to default value
+        }
+
+        [Fact]
+        public void NullArray_GetArrayDataReference()
+        {
+            int[]? arr = null;
+            Assert.Throws<NullReferenceException>(() => { ref int r = ref MemoryMarshal.GetArrayDataReference(arr); });
+        }
+
+        [Fact]
+        public void MultiDimensionalArray_GetArrayDataReference()
+        {
+            int[,] arr = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            ref int r = ref Unsafe.As<byte, int>(ref MemoryMarshal.GetArrayDataReference(arr));
+            Assert.Equal(1, r);
+            ref int r2 = ref Unsafe.Add(ref r, 1);
+            Assert.Equal(2, r2);
+            ref int r3 = ref Unsafe.Add(ref r, 2);
+            Assert.Equal(3, r3);
+            ref int r4 = ref Unsafe.Add(ref r, 3);
+            Assert.Equal(4, r4);
+            Unchecked.Write(ref r, 0, 10);
+            Assert.Equal(10, arr[0, 0]);
+            Unchecked.Write(ref r, 1, 11);
+            Assert.Equal(11, arr[0, 1]);
+            Unchecked.Write(ref r, 2, 12);
+            Assert.Equal(12, arr[1, 0]);
+            Unchecked.Write(ref r, 3, 13);
+            Assert.Equal(13, arr[1, 1]);
+
+            int[,,] arr3D = new int[2, 2, 2] { { { 1, 2 }, { 3, 4 } }, { { 5, 6 }, { 7, 8 } } };
+            ref int r3D = ref Unsafe.As<byte, int>(ref MemoryMarshal.GetArrayDataReference(arr3D));
+            Assert.Equal(1, r3D);
+            Unchecked.Write(ref r3D, 0, 10);
+            Assert.Equal(10, arr3D[0, 0, 0]);
+            Unchecked.Write(ref r3D, 7, 18);
+            Assert.Equal(18, arr3D[1, 1, 1]);
+        }
+
+        [Fact]
+        public void NullMultiDimensionalArray_GetArrayDataReference()
+        {
+            int[,]? arr = null;
+            Assert.Throws<NullReferenceException>(() => { ref int r = ref Unsafe.As<byte, int>(ref MemoryMarshal.GetArrayDataReference(arr)); });
+        }
+
+        [Fact]
+        public void String_GetReference()
+        {
+            string str = "Hello, World!";
+            ref char c = ref Unchecked.GetReference(str);
+            Assert.Equal('H', c);
+        }
+
+        [Fact]
+        public void Array_GetReference()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            ref int r = ref Unchecked.GetReference(arr);
+            Assert.Equal(1, r);
+        }
+
+        [Fact]
+        public void EmptyString_GetReference()
+        {
+            string str = "";
+            ref char c = ref Unchecked.GetReference(str);
+            Assert.Equal('\0', c); // GetReference on empty string returns reference to null terminator
+        }
+
+        [Fact]
+        public void NullString_GetReference()
+        {
+            string? str = null;
+            Assert.Throws<NullReferenceException>(() => { ref char c = ref Unchecked.GetReference(str); });
+        }
+
+        [Fact]
+        public void EmptyArray_GetReference()
+        {
+            int[] arr = [];
+            ref int r = ref Unchecked.GetReference(arr);
+            Assert.Equal(0, r); // GetReference on empty array returns reference to default value
+        }
+
+        [Fact]
+        public void NullArray_GetReference()
+        {
+            int[]? arr = null;
+            Assert.Throws<NullReferenceException>(() => { ref int r = ref Unchecked.GetReference(arr); });
+        }
+
+        [Fact]
+        public void MultiDimensionalArray_GetReference()
+        {
+            int[,] arr = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            ref int r = ref Unchecked.GetReference<int>(arr);
+            Assert.Equal(1, r);
+            ref int r2 = ref Unsafe.Add(ref r, 1);
+            Assert.Equal(2, r2);
+            ref int r3 = ref Unsafe.Add(ref r, 2);
+            Assert.Equal(3, r3);
+            ref int r4 = ref Unsafe.Add(ref r, 3);
+            Assert.Equal(4, r4);
+        }
+
+        [Fact]
+        public void NullMultiDimensionalArray_GetReference()
+        {
+            int[,]? arr = null;
+            Assert.Throws<NullReferenceException>(() => { ref int r = ref Unchecked.GetReference<int>(arr); });
+        }
+
+        [Fact]
+        public void ObjectArray_GetReference()
+        {
+            object[] arr = { "Hello", 123, 3.14 };
+            ref object r = ref Unchecked.GetReference(arr);
+            Assert.Equal("Hello", r);
+            ref object r2 = ref Unsafe.Add(ref r, 1);
+            Assert.Equal(123, r2);
+            ref object r3 = ref Unsafe.Add(ref r, 2);
+            Assert.Equal(3.14, r3);
+        }
+
+        [Fact]
+        public void ObjectArray_Null_GetReference()
+        {
+            object?[] arr = { null, "Hello", 123 };
+            ref object? r = ref Unchecked.GetReference(arr);
+            Assert.Null(r);
+            ref object? r2 = ref Unsafe.Add(ref r, 1);
+            Assert.Equal("Hello", r2);
+            ref object? r3 = ref Unsafe.Add(ref r, 2);
+            Assert.Equal(123, r3);
+        }
+
+        [Fact]
+        public void NonZeroBasedOneDimensionalArray_BehaviorCheck()
+        {
+            Array arr = Array.CreateInstance(typeof(int), new int[] { 2 }, new int[] { 5 });
+            arr.SetValue(42, 5);
+            arr.SetValue(99, 6);
+            ref var r = ref MemoryMarshal.GetArrayDataReference(arr);
+            Assert.Equal(42, Unsafe.As<byte, int>(ref r));
+        }
+
+        [Fact]
+        public void ZeroBasedOneDimensionalArray_BehaviorCheck()
+        {
+            Array arr = Array.CreateInstance(typeof(int), new int[] { 2 }, new int[] { 0 });
+            arr.SetValue(42, 0);
+            arr.SetValue(99, 1);
+            ref var r = ref MemoryMarshal.GetArrayDataReference(arr);
+            Assert.Equal(42, Unsafe.As<byte, int>(ref r));
+        }
+    }
+    #pragma warning restore CS8604, CS8625, CS8601, CS8602
+}
