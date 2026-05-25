4eb0ffb4b215c98db7a347afce28b21c9f4ea50a
andot <mabingyao@gmail.com>
2026-05-25 18:37:31 +0800

    Use void* pointer and nint/nuint overloads
    Change Ptr<T>.Pointer from T* to void* and add explicit casts to T* where needed. Replace int-based pointer arithmetic and indexing with nint/nuint overloads for +, -, and indexers, add corresponding subtraction overloads, and make Ptr-Ptr subtraction return nint. Use Unsafe.SizeOf<T>() when computing element differences and use Unsafe.Add/Unsafe.Subtract with the void* pointer. These changes enable platform-sized offsets and decouple the stored pointer representation from the generic element type.

--- Cool/Unchecked.Ptr.cs
+++ Cool/Unchecked.Ptr.cs
@@ -9,7 +9,7 @@ public static partial class Unchecked
     [StructLayout(LayoutKind.Sequential)]
     public unsafe struct Ptr<T>(T* pointer) where T : unmanaged
     {
-        public T* Pointer = pointer;
+        public void* Pointer = pointer;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public readonly ref T GetReference() => ref Unsafe.AsRef<T>(Pointer);
@@ -17,13 +17,17 @@ public static partial class Unchecked
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator Ptr<T>(T* pointer) => new(pointer);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator T*(Ptr<T> ptr) => ptr.Pointer;
+        public static implicit operator T*(Ptr<T> ptr) => (T*)ptr.Pointer;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Ptr<T> operator +(Ptr<T> ptr, int offset) => new((T*)Unsafe.Add<T>(ptr.Pointer, offset));
+        public static Ptr<T> operator +(Ptr<T> ptr, nint offset) => new((T*)Unsafe.Add<T>(ptr.Pointer, offset));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Ptr<T> operator -(Ptr<T> ptr, int offset) => new((T*)Unsafe.Subtract<T>(ptr.Pointer, offset));
+        public static Ptr<T> operator +(Ptr<T> ptr, nuint offset) => new((T*)Unsafe.Add<T>(ptr.Pointer, offset));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int operator -(Ptr<T> left, Ptr<T> right) => (int)Unsafe.ByteOffset(ref Unsafe.AsRef<T>(right.Pointer), ref Unsafe.AsRef<T>(left.Pointer)) / sizeof(T);
+        public static Ptr<T> operator -(Ptr<T> ptr, nint offset) => new((T*)Unsafe.Subtract<T>(ptr.Pointer, offset));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Ptr<T> operator -(Ptr<T> ptr, nuint offset) => new((T*)Unsafe.Subtract<T>(ptr.Pointer, offset));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static nint operator -(Ptr<T> left, Ptr<T> right) => (nint)Unsafe.ByteOffset(ref Unsafe.AsRef<T>(right.Pointer), ref Unsafe.AsRef<T>(left.Pointer)) / Unsafe.SizeOf<T>();
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Ptr<T> operator ++(Ptr<T> ptr) => new((T*)Unsafe.Add<T>(ptr.Pointer, 1));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -44,7 +48,12 @@ public static partial class Unchecked
         public readonly IntPtr ToIntPtr() => (IntPtr)Pointer;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public readonly UIntPtr ToUIntPtr() => (UIntPtr)Pointer;
-        public readonly ref T this[int index]
+        public readonly ref T this[nint index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref Unsafe.Add(ref Unsafe.AsRef<T>(Pointer), index);
+        }
+        public readonly ref T this[nuint index]
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => ref Unsafe.Add(ref Unsafe.AsRef<T>(Pointer), index);
