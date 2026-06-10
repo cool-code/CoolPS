4923b4c4f77af1599d210a113c99c8bc3210ba14
andot <mabingyao@gmail.com>
2026-06-10 22:18:57 +0800

    Replace ArrayExtensions with NetFxExtensions
    Remove the old Cool/ArrayExtensions.cs and add Cool/NetFxExtensions.cs (NETFRAMEWORK). The new file preserves the array Fill<T> extension methods and consolidates additional .NET Framework compatibility helpers: UIntPtr.MaxValue and Math.Min/Max and IntPtr.Min/Max overloads for pointer-sized types. Centralizes compatibility shims under a single NETFRAMEWORK conditional file.

---
+++ Cool/NetFxExtensions.cs
@@ -0,0 +1,78 @@
+#if NETFRAMEWORK
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Cool;
+
+public static class NetFxExtensions
+{
+    extension(Array)
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Fill<T>(T[] array, T value) => Unchecked.Fill(ref array.GetReference(), (nuint)array.Length, in value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Fill<T>(T[] array, T value, int startIndex, int count) => Unchecked.Fill(ref Unsafe.Add(ref array.GetReference(), startIndex), (nuint)count, in value);
+    }
+    extension(UIntPtr)
+    {
+        public static UIntPtr MaxValue
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ~(nuint)0;
+        }
+    }
+
+    extension(Math)
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe UIntPtr Min(UIntPtr left, UIntPtr right)
+        {
+            if (sizeof(UIntPtr) == 8)
+            {
+                return (UIntPtr)Math.Min((ulong)left, (ulong)right);
+            }
+            else
+            {
+                return (UIntPtr)Math.Min((uint)left, (uint)right);
+            }
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe UIntPtr Max(UIntPtr left, UIntPtr right)
+        {
+            if (sizeof(UIntPtr) == 8)
+            {
+                return (UIntPtr)Math.Max((ulong)left, (ulong)right);
+            }
+            else
+            {
+                return (UIntPtr)Math.Max((uint)left, (uint)right);
+            }
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe IntPtr Min(IntPtr left, IntPtr right)
+        {
+            if (sizeof(IntPtr) == 8)
+            {
+                return (IntPtr)Math.Min((long)left, (long)right);
+            }
+            else
+            {
+                return (IntPtr)Math.Min((int)left, (int)right);
+            }
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe IntPtr Max(IntPtr left, IntPtr right)
+        {
+            if (sizeof(IntPtr) == 8)
+            {
+                return (IntPtr)Math.Max((long)left, (long)right);
+            }
+            else
+            {
+                return (IntPtr)Math.Max((int)left, (int)right);
+            }
+        }
+    }
+}
+
+#endif
